d7093b6da77d2f516a673d1b9f5ed14950c77648
Chaitanya Tyagi <chaitanyatyagi1540@gmail.com>
2022-10-12 20:23:33 +0530

    conflict resln

--- server.js
+++ server.js
@@ -35,9 +35,31 @@ app.get("/", (req, res, next) => {
 
 const authRouter = require("./routes/auth.js");
 const streamRouter = require("./routes/stream.js");
+const phoneVerfiyRouter = require("./routes/phoneVerify");
+const handleCartOps = require("./routes/handleCartOps");
 const productRouter = require("./routes/products.js");
 app.use("/auth", authRouter);
 app.use("/stream", streamRouter);
 app.use("/products", productRouter);
+app.use("/phoneVerify", phoneVerfiyRouter);
+app.use("/handleCartOps", handleCartOps);
 
 app.listen(process.env.PORT || 3000);
+
+/*
+required end points
+for each product -> 
+add to cart --> what quantity 
+{
+    id (int)
+    qnt (int)
+}
+will be added to the cart
+cart structure
+{
+    user_id: (str)
+    id: (int)
+    qnt: (int)
+    valid: (int) -> 0 or 1 ()
+}
+*/
