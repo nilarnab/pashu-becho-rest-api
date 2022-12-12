faae492ee956099e7a64d1ed8345ca8e01680dbf
nilarnab <nilarnabdebnath@gmail.com>
2022-12-13 00:25:03 +0530

    name updation added

--- routes/userInfo.js
+++ routes/userInfo.js
@@ -4,12 +4,57 @@ const Sessions = require("../models/Sessions");
 const Users = require("../models/Users");
 
 
+async function is_session_alive(uuid, user_id) {
+    var sessions = await Sessions.find({ uuid: uuid, user_id: user_id, alive: 1 })
+
+    if (sessions.length) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+router.post('/update_name', async (req, res, next) => {
+
+    console.log(req.query)
+    // updates the name of the user 
+
+    // initialization
+    var uuid = req.query.uuid
+    var user_id = req.query.user_id
+    var name = req.query.name
+
+    // checking if session is alive
+    var session_life = await is_session_alive(uuid, user_id)
+
+    // updating name in the database
+    //------------------------------------------
+    if (session_life) {
+        var response = await Users.findByIdAndUpdate(user_id, { name: name })
+
+        return res.json({
+            verdict: 1,
+            response,
+            message: 'name change to ' + name
+        })
+    }
+    else {
+        return res.json({
+            verdict: 0,
+            message: 'session is not alive'
+        })
+    }
+    //----------------------------------------
+
+
+})
+
 router.post('/get_by_phone', async (req, res, next) => {
-    
-    if (req.query.phone_num)
-    {    
+
+    if (req.query.phone_num) {
         var phone = req.query.phone_num
-        var user = await Users.find({phone: phone})
+        var user = await Users.find({ phone: phone })
 
         res.json(
             {
@@ -19,8 +64,7 @@ router.post('/get_by_phone', async (req, res, next) => {
         )
     }
 
-    else
-    {
+    else {
         res.json({
             verdict: 0,
             message: "Invalid fields"
