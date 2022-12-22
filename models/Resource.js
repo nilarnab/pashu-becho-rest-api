ee943d0212225404666b168281c870a943451ece
Vishnu Mali <vishnumali3911@gmail.com>
2022-12-22 15:06:33 +0530

    done

--- models/Resource.js
+++ models/Resource.js
@@ -17,10 +17,10 @@ const ResourceSchema = new mongoose.Schema({
 
 ResourceSchema.virtual('url').get(function(){
     if(this.type==="image"){
-        return 'https://'
+        return 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-collection-1_large.png?format=webp&v=1530129113'
     }
     else if(this.type=="video"){
-        return 'https://'
+        return 'http://159.223.90.95:5000/video/id_video_1/_manifest.mpd'
     }
     else{
         return ''
