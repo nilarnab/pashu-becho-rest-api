a3dfaecc29a2a1bde6d57b6b402b869b27db45b6
Vishnu Mali <vishnumali3911@gmail.com>
2022-12-22 15:22:57 +0530

    temp route

--- models/Resource.js
+++ models/Resource.js
@@ -4,15 +4,15 @@ const ResourceSchema = new mongoose.Schema({
     type:String,
     productID:String,
     active:Boolean,
-    // url:{type:String,default:function(){ if(this.type==="image"){
-    //     return 'https://'
-    // }
-    // else if(this.type=="video"){
-    //     return 'https://'
-    // }
-    // else{
-    //     return ''
-    // } }}
+    url:{type:String,default:function(){ if(this.type==="image"){
+        return 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-collection-1_large.png?format=webp&v=1530129113'
+    }
+    else if(this.type=="video"){
+        return 'http://159.223.90.95:5000/video/id_video_1/_manifest.mpd'
+    }
+    else{
+        return ''
+    } }}
 });
 
 ResourceSchema.virtual('url').get(function(){
